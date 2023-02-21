03a44dc80c1b8ea92fc149aade06a021999e1f4e
Valentyn310 <53212254+Valentyn310@users.noreply.github.com>
2023-02-21 18:46:33 +0200

    add

--- js/main.js
+++ js/main.js
@@ -499,3 +499,37 @@ document.getElementById("tg-back").addEventListener("submit", function (e) {
 $(function () {
     $('input[type="tel"]').mask('+38(000)000-00-00');
 });
+
+
+// go-top
+// считываем кнопку
+const goTopBtn = document.querySelector(".go-top");
+
+// обработчик на скролл окна
+window.addEventListener("scroll", trackScroll);
+// обработчик на нажатии
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+  // вычисляем положение от верхушки страницы
+  const scrolled = window.pageYOffset;
+  // считаем высоту окна браузера
+  const coords = document.documentElement.clientHeight;
+  // если вышли за пределы первого окна
+  if (scrolled > coords) {
+    // кнопка появляется
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    // иначе исчезает
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+// function goTop() {
+//   // пока не вернулись в начало страницы
+//   if (window.pageYOffset > 0) {
+//     // скроллим наверх
+//     // window.scrollBy(0, -75); // второй аргумент - скорость
+//     setTimeout(goTop, 0); // входим в рекурсию
+//   }
+// }
